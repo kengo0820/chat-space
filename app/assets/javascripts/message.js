b405578bad22ea18271284847b9682f614b4ec8f
kengo0820 <ken0820go.s@gmail.com>
2019-11-30 15:55:10 +0900

    提出１

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,44 @@
 $(function(){
   function buildHTML(message){
+
+
     if (message.image) {
       var html = `<div class="message">
-                  <div class="upper-info">
-                  <div class="upper-info__talker">
-                    ${message.name}
-                  </div>
-                  <div class="upper-info__date">
-                    ${message.time}
-                  </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  <img class="lower-message__image" src="/uploads/message/image/${message.user_id}/IMG_3960.png" alt="Img 3960">
-                  </div>
+                    <div class="upper-info">
+                      <div class="upper-info__talker">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-info__date">
+                        ${message.time}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <img class="lower-message__image" src = ${message.image}>
+                    </div>
                   </div>`
     } else {
       var html = `<div class="message">
-                  <div class="upper-info">
-                  <div class="upper-info__talker">
-                    ${message.name}
-                  </div>
-                  <div class="upper-info__date">
-                    ${message.time}
-                  </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  </div>
+                    <div class="upper-info">
+                      <div class="upper-info__talker">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-info__date">
+                        ${message.time}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
                   </div>`
     }
     return html;
   }
   $("#new_message").on('submit', function(e){
-    console.log("OK")
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -55,9 +56,10 @@ $(function(){
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('#new_message')[0].reset();
       $('input').prop('disabled', false);
-    });
+    })
+
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('error')
       $('input').prop('disabled', false);
     });
  
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.content  @message.content
 json.user_id  @message.user.id
 json.user_name  @message.user.name
 json.time @message.created_at
+json.image @message.image_url
