8c4765a0dcdf45d713bbc6016b26014544eee48f
kengo0820 <ken0820go.s@gmail.com>
2019-12-07 05:20:51 +0900

    保存

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ def update
 end
 
 def index
+  return nil if params[:keyword] == ""
+  @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
   respond_to do |format|
     format.html
     format.json
